230636a6992c4f5772093bc80c4ab890626fea6f
Yann Amsellem <10496045+yannamsellem@users.noreply.github.com>
2026-08-07 11:06:22 +0200

    chore: bootstrap project

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import reactDom from "eslint-plugin-react-dom";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactX from "eslint-plugin-react-x";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs.flat["recommended-latest"],
+      reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
+      prettier,
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: globals.browser,
+    },
+  },
+]);
