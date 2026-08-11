2b63e959b110db524d87b5262d1c7a494cda2796
Yann Amsellem <10496045+yannamsellem@users.noreply.github.com>
2026-08-11 02:38:37 +0200

    chore: deploy with cloudflare workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
